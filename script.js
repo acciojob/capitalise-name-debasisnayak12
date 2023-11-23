d654b966c59f65238a506c6a8aed3d1d654b0cc0
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-23 19:14:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const inputField = document.querySelector("#fname");
+
+inputField.addEventListener(('blur'), () => {
+	const inputValue = inputField.value;
+	inputField.value = inputValue.toUpperCase();
+});
+
